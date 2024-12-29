490af21595cbf17499789ded18e0db53800666c9
boris aushev <borisaushev.git@gmail.com>
2024-12-29 17:29:52 +0300

    ТЗ-4

--- ntoproject/src/main/java/com/ntoproject/editables/ExhibitionItem.java
+++ ntoproject/src/main/java/com/ntoproject/editables/ExhibitionItem.java
@@ -4,6 +4,7 @@ import com.futurefactory.Data;
 import com.futurefactory.Wrapper;
 import com.futurefactory.editor.EditorEntry;
 import com.futurefactory.editor.EditorEntryBase;
+import com.ntoproject.editables.space.Center;
 import com.ntoproject.editables.space.CenterSpace;
 import com.ntoproject.editables.space.Owner;
 
@@ -12,33 +13,24 @@ import java.lang.reflect.Field;
 import java.util.function.Supplier;
 
 public class ExhibitionItem extends Data.Editable {
+    @EditorEntry(translation = "Название")
+    public String name;
+    @EditorEntry(translation = "Владелец", editorBaseSource = OwnerSelector.class)
+    public Owner owner;
+
     public ExhibitionItem() {
         super("Новый экспонат");
     }
 
-    @EditorEntry(translation = "Название")
-    public String name;
-
-    //@EditorEntry(translation = "Владелец", editorBaseSource = OwnerSelector.class)
-    public Owner owner;
-/*
     public static class OwnerSelector implements EditorEntryBase {
-        public JComponent createEditorBase(Object o, Field f, Wrapper<Runnable> saver){
-            JComboBox<WorkArea>c=new JComboBox<>();
-            for(Workshop s:Data.getInstance().getGroup(Workshop.class)){
-                for(WorkArea w:s.parts)c.addItem(w);
-            }
-            try{c.setSelectedItem(f.get(o));}catch(IllegalAccessException ex){throw new RuntimeException(ex);}
-            saver.var=()->{try{f.set(o,c.getSelectedItem());}catch(IllegalAccessException ex){throw new RuntimeException(ex);}};
-            return c;
-        }
-
         @Override
         public JComponent createEditorBase(Object o, Field field, Wrapper<Supplier<?>> wrapper) {
-            JComboBox<Owner>c=new JComboBox<>();
-            for(CenterSpace s:Data.getInstance().getGroup(CenterSpace.class)){
-                for(CenterSpace w:s.parts)c.addItem(w);
-            }
+            JComboBox<Owner> c = new JComboBox<>();
+            for (CenterSpace s : Data.getInstance().getGroup(CenterSpace.class))
+                c.addItem(s);
+            c.addItem(Center.getInstance());
+            wrapper.var = c::getSelectedItem;
+            return c;
         }
-    }*/
+    }
 }
--- ntoproject/src/main/java/com/ntoproject/editables/space/Center.java
+++ ntoproject/src/main/java/com/ntoproject/editables/space/Center.java
@@ -1,10 +1,15 @@
 package com.ntoproject.editables.space;
 
 public class Center implements Owner {
-
     private Center(){}
+
+    private final static Center instance = new Center();
+    public static Center getInstance() {
+        return instance;
+    }
+
     @Override
     public String getName() {
-        return "(Культурный центр";
+        return "Культурный центр";
     }
 }
